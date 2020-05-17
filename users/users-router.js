3ac9f24eb4a1a534dd10bf047bdaa14460947cc1
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-16 18:11:50 -0700

    check up the existed user by their username and then by password

--- users/users-router.js
+++ users/users-router.js
@@ -23,14 +23,35 @@ router.post("/register", async (req, res, next) => {
     user.password = hash; // we assigned the user.password to the hash that we just
     // generated
     const newUser = await Users.add(user);
-    // even if we create a new user with the same password the hash would be 
+    // even if we create a new user with the same password the hash would be
     // different because bcryptjs using something called salt
     // salt is just a random string that add to the password that we provide and combined
     // them and generate the hashed password
     // which means if 2 users have the same password the salt will make them different
     // and this is so important for security.
     res.status(201).json(newUser);
+  } catch (err) {
+    next(err);
+  }
+});
 
+router.post("/login", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const existedUser = await Users.findBy({ username }).first();
+    if (
+      existedUser &&
+      bcrypt.compareSync(
+        password /*  this is the guessed password
+      that the user try to type in */,
+        existedUser.password /* and this is the existed password
+      that been hashed before since we already have access to the db */
+      )
+    ) {
+      res.status(200).json({ message: `welcome ${existedUser.username}` });
+    } else {
+      res.status(401).json({ message: "Invalid credentials" });
+    }
   } catch (err) {
     next(err);
   }
