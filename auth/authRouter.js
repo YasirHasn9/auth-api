b95d4f924961a5c36384f9fb1917bcb0a6d96b1a
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-17 13:13:14 -0700

    send cookies to user browser

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -20,6 +20,11 @@ router.post("/login", (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        //   after our library configured correctly and hashed in the password in db
+        // we can set anything on our session 
+        req.session.user = user // we are telling the lib that we are saving the 
+        // information about this user in the session go head save that and send the cookie 
+        // with that user info
         res.status(200).json({ message: `Welcome ${user.username}` });
       } else {
           res.status(401).json({message: "Invalid credentials"})
