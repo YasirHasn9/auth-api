9f3ec09c121e711ba91f1990da86406509de69c7
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-17 12:30:35 -0700

    register a new user

--- api/server.js
+++ api/server.js
@@ -6,40 +6,40 @@ const cors = require("cors");
 const session = require("express-session");
 
 const usersRouter = require("../users/users-router.js");
+const authRouter = require("../auth/authRouter");
 
 const server = express();
-const sessionConfig  = {
+const sessionConfig = {
   /* 
   name and secret is for the developer work 
   */
-  name: "monkey" , //  we need to named something that would helps to make the attackers
+  name: "monkey", //  we need to named something that would helps to make the attackers
   // confused to guess because the name be default is sid: By exposed this to them
   // it would be easy to guess the library that we use and easily get access to
   // our database
-  secret: "keep it save , keep it secret!" , // this what we are gonna use in order to encrypt and decrypt the 
-  // cookies by the server by the server , so on every request the server will 
+  secret: "keep it save , keep it secret!", // this what we are gonna use in order to encrypt and decrypt the
+  // cookies by the server by the server , so on every request the server will
   // look up for the secret to check if is it valid or not
 
   /*
   this is for the cookies itself
   */
- cookies: {
-   maxAge:1000 * 30 , //  this is how long we want the cookies would be 
-  //  stored in the db
-  secure:false , // this is a boolean value , which asking , is it ok that we send
-  // cookie over an encrypted connection ? over https 
-  // during development ,it is ok to set to false  because we are not setting up 
-  // https but through production we can set it to true using var in the env
-  // what is an encrypted connection ? its encrypted exchange between the website
-  // we are visiting and the internet
-  httpOnly:true , // means that this cookie can not be accessed by js
- },
- resave:false, // do we want recreate a session even it has not changed
- saveUninitialized:false // this is asking the client if it is okey 
-//  to save the cookies on their website locally because there is laws against saving
-// the cookies
-
-}
+  cookies: {
+    maxAge: 1000 * 30, //  this is how long we want the cookies would be
+    //  stored in the db
+    secure: false, // this is a boolean value , which asking , is it ok that we send
+    // cookie over an encrypted connection ? over https
+    // during development ,it is ok to set to false  because we are not setting up
+    // https but through production we can set it to true using var in the env
+    // what is an encrypted connection ? its encrypted exchange between the website
+    // we are visiting and the internet
+    httpOnly: true // means that this cookie can not be accessed by js
+  },
+  resave: false, // do we want recreate a session even it has not changed
+  saveUninitialized: false // this is asking the client if it is okey
+  //  to save the cookies on their website locally because there is laws against saving
+  // the cookies
+};
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
@@ -47,9 +47,10 @@ server.use(cors());
 server.use(session(sessionConfig));
 
 server.use("/api/users", usersRouter);
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
-});
+})
 
 module.exports = server;
--- auth/authRouter.js
+++ auth/authRouter.js
@@ -1,30 +1,16 @@
+const router = require("express").Router();
 const bcrypt = require("bcryptjs");
+
 const Users = require("../users/users-model");
 
-module.exports = (req, res, next) => {
-  // this is will send on the http through the headers
-  const { username, password } = req.headers;
+router.post("/register", (req, res) => {
+  const user = req.body;
+  // user has username and password
+  const hash = bcrypt.hashSync(user.password, 12);
+  user.password = hash; // we hashed before we send to the db
+  Users.add(user)
+    .then(savedUser => res.json(savedUser))
+    .catch(err => res.status(500).json(err));
+});
 
-  if ((username, password)) {
-    // if we get them right then we have a user
-    // find the user using the username in the db
-    Users.findBy({ username })
-      .first() // return on object contained the user form the db
-      .then(user => {
-        //   when we use the compareSync,
-        //  it means we hashed the password already in the db
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next(); // allows for the user to play with the db
-        } else {
-          res.status(401).json({
-            message: "Invalid credentials"
-          });
-        }
-      })
-      .catch(err => {
-        res.status(500).json({ message: "Ran into an expected error" });
-      });
-  } else {
-    res.status(400).json({ message: "No credentials provided" });
-  }
-};
+module.exports = router;
