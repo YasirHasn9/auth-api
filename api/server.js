3873f2b15ccfcf09fde47294a7305577c837c319
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-16 17:58:29 -0700

    genrate a hashed password

--- api/server.js
+++ api/server.js
@@ -3,7 +3,6 @@ const helmet = require("helmet");
 const cors = require("cors");
 
 const usersRouter = require("../users/users-router.js");
-const authRouter = require("../auth/auth-router");
 
 const server = express();
 
@@ -12,7 +11,6 @@ server.use(express.json());
 server.use(cors());
 
 server.use("/api/users", usersRouter);
-server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.json({ api: "up" });
--- users/users-router.js
+++ users/users-router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const bcrypt = require("bcryptjs"); // this is gonna generate a hashed password
 const Users = require("./users-model.js");
 
 router.get("/", (req, res) => {
@@ -10,4 +10,29 @@ router.get("/", (req, res) => {
     .catch(err => res.send(err));
 });
 
+router.post("/register", async (req, res, next) => {
+  try {
+    // firsts we have to hash the password of the user using bcryptjs
+    let user = req.body;
+
+    // this is a sync function , which means it will stop here then gonna continue
+    // after it hashed the whole password for 15 rounds
+    // in this case hackers can not guess our password
+    let hash = bcrypt.hashSync(user.password, 15);
+
+    user.password = hash; // we assigned the user.password to the hash that we just
+    // generated
+    const newUser = await Users.add(user);
+    // even if we create a new user with the same password the hash would be 
+    // different because bcryptjs using something called salt
+    // salt is just a random string that add to the password that we provide and combined
+    // them and generate the hashed password
+    // which means if 2 users have the same password the salt will make them different
+    // and this is so important for security.
+    res.status(201).json(newUser);
+
+  } catch (err) {
+    next(err);
+  }
+});
 module.exports = router;
