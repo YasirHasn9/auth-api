774938c66d7da6bfc5a4ffcdf06acf85eead67dc
YasirHasn9 <hassanyasir223@gmail.com>
2020-05-17 12:35:28 -0700

    login

--- auth/authRouter.js
+++ auth/authRouter.js
@@ -13,4 +13,19 @@ router.post("/register", (req, res) => {
     .catch(err => res.status(500).json(err));
 });
 
+router.post("/login", (req, res) => {
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.username}` });
+      } else {
+          res.status(401).json({message: "Invalid credentials"})
+      }
+    }).catch(err =>{
+        res.status(500).json(err)
+    })
+});
 module.exports = router;
